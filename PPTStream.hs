ef5835aa4de2ed3eb062ee5f2beeb2d9a0ed5932
Sergey Kishchenko <voidwrk@gmail.com>
2010-02-03 18:09:09 +0200

    PPTRecordHeader was added

--- PPTStream.hs
+++ PPTStream.hs
@@ -3,8 +3,31 @@ module PPTStream where
 import List
 import Maybe
 import OLEStorage
+import Data.Binary
+import Data.Binary.Get as BinaryGet
+import Data.Bits
 import qualified Data.ByteString.Lazy as B
 
+-- data types -------------------------------------------------------------------------
+data PPTRecordHeader = PPTRecordHeader { recVer         :: Int, -- first 4 bits
+                                         recInstance    :: Int, -- next 12 bits
+                                         recType        :: Word16, -- next 2 bytes, should be converted later
+                                         recLen         :: Word32 -- next 4 bytes 
+                                       }
+-- end of data types ------------------------------------------------------------------
+-- instances --------------------------------------------------------------------------
+instance Binary PPTRecordHeader where
+  put = undefined
+  get = do verAndInstance <- BinaryGet.getWord16le
+           recType <- BinaryGet.getWord16le
+           recLen <- BinaryGet.getWord32le
+           return PPTRecordHeader { recVer=shiftR 12 (fromIntegral (verAndInstance .&. 0xf000)),
+                                    recInstance=fromIntegral $ verAndInstance .&. 0x0fff,
+                                    recType=recType,
+                                    recLen=recLen
+                                  }
+-- end of instances--------------------------------------------------------------------
+-- functions --------------------------------------------------------------------------
 parsePPTStream = parseDocument
 
 streamHeaderInfo :: OLEDocument -> String
