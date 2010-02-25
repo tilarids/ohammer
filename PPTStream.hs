e43ae87fd0e19b0f8095bc10bd3ec6ca51c90801
Sergey Kishchenko <voidwrk@gmail.com>
2010-02-25 14:44:55 +0200

    PPT Parsing - work in progress

--- PPTStream.hs
+++ PPTStream.hs
@@ -7,6 +7,7 @@ import Data.Binary
 import Data.Binary.Get as BinaryGet
 import Data.Bits
 import qualified Data.ByteString.Lazy as B
+import Control.Monad.Loops
 
 -- data types -------------------------------------------------------------------------
 data PPTRecordHeader = PPTRecordHeader { recVer         :: Int, -- first 4 bits
@@ -14,6 +15,13 @@ data PPTRecordHeader = PPTRecordHeader { recVer         :: Int, -- first 4 bits
                                          recType        :: Word16, -- next 2 bytes, should be converted later
                                          recLen         :: Word32 -- next 4 bytes 
                                        }
+
+data PPTNode = PPTContainer { recordHeader                   :: PPTRecordHeader, -- header
+                              childrenNodes                  :: [PPTNode]  -- children
+                              } |
+               PPTAtom      { recordHeader                   :: PPTRecordHeader, -- header
+                              nodeBinary                     :: B.ByteString -- binaryData
+                            }
 -- end of data types ------------------------------------------------------------------
 -- instances --------------------------------------------------------------------------
 instance Binary PPTRecordHeader where
@@ -26,8 +34,42 @@ instance Binary PPTRecordHeader where
                                     recType=recType,
                                     recLen=recLen
                                   }
+
+-- readPPTChildren :: PPTRecordHeader -> Word32 -> Get (Maybe (PPTNode, Word32))
+-- readPPTChildren recHeader acc = if acc == (recLen recHeader)
+--                                    then Get Nothing
+--                                    else do node <- get
+--                                            node :: PPTNode
+--                                            return Just (node, acc + recLen (recordHeader node))
+
+-- readPPTChildren2 :: PPTRecordHeader -> [Get PPTNode]
+-- readPPTChildren2 header = impl 0 []
+--     where impl total l
+--             | total == (recLen header) = reverse l
+--             | otherwise = do node <- get
+--                              node :: PPTNode
+--                              return (impl (total + recLen (recordHeader node)) (node : l))
+--           impl :: Word32 -> [PPTNode] -> [PPTNode]
+
+instance Binary PPTNode where
+  put = undefined
+  get = do recHeader <- get
+           if isContainer recHeader
+              then do return PPTContainer { recordHeader=recHeader,
+                                            childrenNodes=[]
+                                          }
+              else do nodeBinary <- BinaryGet.getLazyByteString $ fromIntegral (recLen recHeader)
+                      return PPTAtom { recordHeader=recHeader,
+                                       nodeBinary=nodeBinary
+                                     }
+           return PPTContainer {recordHeader=recHeader,
+                                childrenNodes=[]}
 -- end of instances--------------------------------------------------------------------
 -- functions --------------------------------------------------------------------------
+isContainer :: PPTRecordHeader -> Bool
+isContainer header = isCont && (0x0428 == recType header) -- old (pre SP1) PP2007 bug: RT_RoundTripCustomTableStyles12Atom has rec_ver==0xF
+    where isCont = 0xF == recVer header
+
 parsePPTStream = parseDocument
 
 streamHeaderInfo :: OLEDocument -> String
@@ -36,6 +78,9 @@ streamHeaderInfo (OLEDocument header _) = show header
 streamDirectoryInfo :: OLEDocument -> String
 streamDirectoryInfo doc = show (entries dir)
     where dir = getDirectory doc
+
+
+
 
 extractEntry file name = B.take (fromIntegral (streamSize entry)) (getBytes entry)
     where doc = parseDocument file
